21dbb04aeadd99fc44345825f0fe1bd467d8703b
farizalresat <farizalresat@gmail.com>
2020-05-09 12:18:33 +0800

    can post

--- Postlist.py
+++ Postlist.py
@@ -5,7 +5,7 @@ username = "farizal"
 account = Account(username)
 c_list = {}
 count = 1
-f = open("test.txt",'w',encoding="utf-8")
+f = open("hivepostlist.md",'w',encoding="utf-8")
 for c in map(Comment, account.history(only_ops=["comment"])):
     if c.permlink in c_list:
       continue
@@ -15,8 +15,13 @@ for c in map(Comment, account.history(only_ops=["comment"])):
          continue
     c_list[c.permlink] = 1
     if not c.is_comment():
-        print("%d " % count)
-        count +=1
-        f.write("[" + c.title + "]" + "(" +"https://steemit.com/"+ c.parent_permlink + "/@"+username+"/"+ c.permlink + ")" + "\n")
+         title = ""
+         title.join(c.title.splitlines())
+         print("%d " % count)
+         count +=1
+         f.write(str(count) + ". [" + title + "]" +
+         "(" +"https://hive.blog/"+
+          c.parent_permlink + "/@"+username+"/"+
+           c.permlink + ")" + "\n")
 f.close()
 print("DONE")
